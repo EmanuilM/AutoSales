8d1ae1b14c673ce5ae7692b8c6b171721f6e5fe9
ExGiX <emanuilmladenov7@gmail.com>
2021-07-09 17:44:31 +0300

    update

---
+++ server/middlewares/isAuthenticated.js
@@ -0,0 +1,10 @@
+const cloudinary = require("cloudinary").v2;
+
+
+module.exports = (req, res, next) => {
+    if (!req.user) {
+        cloudinary.api.delete_resources(req.body.imageIds);
+        return res.status(401).json({message : "Unauthorized"});
+    }
+    next();
+}
