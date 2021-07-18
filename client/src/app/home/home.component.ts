dc19e91a8b3f87c1d400b35850e1b7a7bcc8f2fb
ExGiX <emanuilmladenov7@gmail.com>
2021-07-19 02:04:50 +0300

    update home page for logged in users

--- client/src/app/home/home.component.ts
+++ client/src/app/home/home.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from '../services/user.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
-
-  constructor() { }
+export class HomeComponent implements OnInit  {
+  get isAuth() : Boolean { 
+    return this.userService.isAuth;
+  }
+  constructor(private userService : UserService) { }
+ 
 
   ngOnInit(): void {
+   
   }
 
 }
