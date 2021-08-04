cba7571f84f6c663f2f747b0de06977963683871
ExGiX <emanuilmladenov7@gmail.com>
2021-08-04 21:28:39 +0300

    update

--- client/dist/client/polyfills.js
+++ client/dist/client/polyfills.js
@@ -7,7 +7,7 @@
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-module.exports = __webpack_require__(/*! C:\Angular\Auto Sales\client\src\polyfills.ts */"hN/g");
+module.exports = __webpack_require__(/*! C:\Users\emanu\OneDrive\Desktop\github\AutoSales\client\src\polyfills.ts */"hN/g");
 
 
 /***/ }),
